cdd55012151fa2dc35f27d75b5c443e0668b992b
EnricoBaroni <enricobaroni@hotmail.es>
2022-09-20 15:49:00 +0200

    DAL y conexion creados

---
+++ ProyectoFinalASP/DAL/DbConnect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalASP.DAL
+{
+    public class DbConnect
+    {
+        SqlConnection miCnx = null;
+        public SqlConnection MiCnx { get => miCnx; set => miCnx = value; }
+
+        public DbConnect()
+        {
+            MiCnx = new SqlConnection(
+    "Data Source=46.183.116.173,54321;Initial Catalog=GrupoRutas;User ID=GrupoRutas;Password=4e5r448r5g585op265;");
+            MiCnx.Open();
+        }
+    }
+}
