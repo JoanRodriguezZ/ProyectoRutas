1a88c677a658df87ce1c53c3c8e68f2146be07c4
joanrod <joanrodz@hotmail.com>
2022-09-21 17:33:50 +0200

    Error con los nombres de archivo

--- ProyectoFinalASP/RutaSeleccionada.aspx.cs
+++ ProyectoFinalASP/RutaSeleccionada.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace ProyectoFinalASP
 {
-    public partial class Ruta : Page
+    public partial class RutaSeleccionada : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- ProyectoFinalASP/RutaSeleccionada.aspx.designer.cs
+++ ProyectoFinalASP/RutaSeleccionada.aspx.designer.cs
@@ -11,7 +11,7 @@ namespace ProyectoFinalASP
 {
 
 
-    public partial class Ruta
+    public partial class RutaSeleccionada
     {
     }
 }
